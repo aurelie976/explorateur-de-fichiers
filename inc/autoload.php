e6e20b7215fd5e0e9fbf512c02cec58b53577be0
DesireeBesnard <desiree.besnard@gmaail.com>
2019-11-27 09:27:24 +0000

    delete twig and composer

--- inc/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('config.php');
-
-function __autoload($class_name = null) {
-
-    $class =explode("_"), $class_name);
-    $path = implode("/", )".php"};
-    @require_once($path);
