f0f951845b6326cf36ec0fd1909ed070531f7174
ericc70 <eric.c@codeur.online>
2019-11-27 08:39:46 +0000

    commit for mise en commun

--- testphp.php
+++ testphp.php
@@ -1,23 +1,81 @@
 <?php
-$base="/var/www/html/";
-if (isset($_GET['d']) ){ $base='/var/www/html/'.$_GET['d'];}
+$page=array('site', 'css' , 'scss,' ,'js' ,'', 'media');
 
-$iterator = new DirectoryIterator($base);
-// liste les dossiers avec return lien
-foreach ($iterator as $fileInfo) {
-// foreach (new DirectoryIterator($base) as $fileInfo) {
-     if (isset($_GET['d']) ){ $url=$_GET['d']."/".$fileInfo->getFilename();}else{$url=$fileInfo->getFilename();}
-    if($fileInfo->isDir()){
-    echo  "<a href='?d=".$url."'>". $fileInfo->getFilename() . "</a><br>\n";
-}
-}
 
-foreach ($iterator as $fileinfo) {
-    if ($fileinfo->isFile()) {
-        echo  $fileinfo->getFilename(). "<br /> \n";
-        // echo  $fileinfo->getFilename().".". $fileinfo->getExtension() . "<br /> \n";
+
+$extFiles = array(
+    '' => "/media/00.png",
+    'doc' => "/media/doc.png",
+    'xls' => "/media/doc.png",
+    'ppt' => "/media/doc.png",
+    'pdf' => "/media/doc.png",
+    'zip' => "/media/doc.png",
+    'txt' => "/media/doc.png",
+    'php' => "/media/03.png",
+    'gif' => "/media/doc.png",
+    'jpg' => "/media/01.png",
+    'png' => "/media/doc.png",
+    'html' => "/media/03.png",
+    'css' => "/media/02.png",
+    'js' => "/media/01.png"
+  );
+
+
+
+
+
+$base="/var/www/html/site/";
+if (isset($_GET['d']) ){
+    $geturl=strip_tags($_GET['d']);
+    $base='/var/www/html/site/'.$geturl;}
+    else{
+
+        $geturl='';
+    
     }
+    
+// on controle les
+if ( in_array($geturl, $page ) && file_exists("/var/www/html/site/". $geturl )){
+//
+
+
+    $iterator = new DirectoryIterator($base);
+    // liste les dossiers avec return lien
+    foreach ($iterator as $fileInfo) {
+    // foreach (new DirectoryIterator($base) as $fileInfo) {
+        if (isset($_GET['d']) ){ $url=strip_tags($_GET['d']).$fileInfo->getFilename();}else{$url=$fileInfo->getFilename();}
+        if($fileInfo->isDir()){
+        echo  "<a href='?d=".$url."'>". $fileInfo->getFilename() . "</a><br>\n";
+       
+    }
+    }
+    //liste les fichiers
+    foreach ($iterator as $fileinfo) {
+        
+        if ($fileinfo->isFile()) {
+            // print_r($fileinfo->getExtension());
+            if(array_key_exists($fileinfo->getExtension() , $extFiles )) {
+                //var_dump(array_key_exists($fileinfo->getExtension() , $extFiles  ));
+            // echo "ok <br>";
+            echo "<p><img src=".$extFiles[$fileinfo->getExtension()].">". $fileinfo->getFilename()."</p>";
+            } else {
+            // echo "no <br>";
+            // var_dump(array_key_exists($fileinfo->getExtension() , $extFiles  ));
+
+            }
+        //  echo  $fileinfo->getFilename(). "<br /> \n";
+            // echo  $fileinfo->getFilename().".". $fileinfo->getExtension() . "<br /> \n";
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 }
-
-
 
